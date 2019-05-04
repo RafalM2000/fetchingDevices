e8b11662597cd98d924d7f462496f0606082d4f8
Rafal <rmf@europe.com>
2019-05-04 08:34:51 +0200

    fetching diffent devices for different customers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component} from '@angular/core';
 import { of } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { callbackify } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +12,12 @@ export class AppComponent  {
   constructor() {};
 
   title = 'FetchingDevices'; 
-  installations = of('Poznań', 'Lusowo', 'Wrocław');
+  customers = ["Jon Doe", 'Jack Daniels', 'Nora Roberts']
+  installations = of();
+  subscribeCustomer = {};
   devices = [
     {installatinId: 'Poznań', devicesId: [342, 333, 612]},
-    {installatinId: 'Jelenia  Góra', devicesId: [3342, 3433, 5612]},
+    {installatinId: 'Jelenia Góra', devicesId: [3342, 3433, 5612]},
     {installatinId: 'Lusowo', devicesId: [103, 56, 934, 384]},
     {installatinId: 'Komorowo', devicesId: [13, 356, 954, 3184]},
     {installatinId: 'Warszawa', devicesId: [13, 5, 93, 7384]},
@@ -26,21 +25,32 @@ export class AppComponent  {
     {installatinId: 'Wrocław', devicesId: [238, 241, 823, 273]}
 ]
 
-devicesItems = [];
-installationItems = [];
+  devicesItems = [];
+  installationItems = [];
 
-  subscribeCustomer = this.installations.subscribe(
-    (instId: string) => {
-      this.fetchDevices(instId);
-    });
 
   fetchDevices(installationId) {
+
     for (let i=0; i < this.devices.length; i++) {
       if (this.devices[i].installatinId === installationId) {
         this.devicesItems[i] = this.devices[i].devicesId;
         this.installationItems[i] = this.devices[i].installatinId;
-        // console.log(this.devices[i].installatinId,'------>', this.devices[i].devicesId);
       }
     }
   }
+  onChange(customer) {
+
+  this.devicesItems = [];
+  this.installationItems = [];
+  if (this.customers[0] === customer) {this.installations = of('Poznań', 'Lusowo', 'Wrocław');};
+  if (this.customers[1] === customer) {this.installations = of('Warszawa', 'Wrocław', 'Jelenia Góra');};
+  if (this.customers[2] === customer) {this.installations = of('Poznań', 'Komorowo', 'Brzeg', 'Lusowo');}
+
+  this.subscribeCustomer = this.installations.subscribe(
+    (instId: string) => {
+      this.fetchDevices(instId);
+    });
+
+}
+
 }
